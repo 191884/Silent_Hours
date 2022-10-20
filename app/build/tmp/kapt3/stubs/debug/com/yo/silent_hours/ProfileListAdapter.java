9ac1fd81f51cf6296e35fb85f5158a649b3ea031
Dr.Aman <Dr.AmanNavodian@gmail.com>
2022-10-20 15:24:55 +0530

    With Activity MVVM Structure

---
+++ app/build/tmp/kapt3/stubs/debug/com/yo/silent_hours/ProfileListAdapter.java
@@ -0,0 +1,64 @@
+package com.yo.silent_hours;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000,\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\u0018\u0000 \u000e2\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001:\u0002\u000e\u000fB\u0005\u00a2\u0006\u0002\u0010\u0004J\u0018\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\u00032\u0006\u0010\b\u001a\u00020\tH\u0016J\u0018\u0010\n\u001a\u00020\u00032\u0006\u0010\u000b\u001a\u00020\f2\u0006\u0010\r\u001a\u00020\tH\u0016\u00a8\u0006\u0010"}, d2 = {"Lcom/yo/silent_hours/ProfileListAdapter;", "Landroidx/recyclerview/widget/ListAdapter;", "Lcom/yo/silent_hours/database/Profile;", "Lcom/yo/silent_hours/ProfileListAdapter$ViewHolder;", "()V", "onBindViewHolder", "", "holder", "position", "", "onCreateViewHolder", "parent", "Landroid/view/ViewGroup;", "viewType", "Companion", "ViewHolder", "app_debug"})
+public final class ProfileListAdapter extends androidx.recyclerview.widget.ListAdapter<com.yo.silent_hours.database.Profile, com.yo.silent_hours.ProfileListAdapter.ViewHolder> {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.yo.silent_hours.ProfileListAdapter.Companion Companion = null;
+    private static final androidx.recyclerview.widget.DiffUtil.ItemCallback<com.yo.silent_hours.database.Profile> PROFILE_COMPARATOR = null;
+    
+    public ProfileListAdapter() {
+        super(null);
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public com.yo.silent_hours.ProfileListAdapter.ViewHolder onCreateViewHolder(@org.jetbrains.annotations.NotNull()
+    android.view.ViewGroup parent, int viewType) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onBindViewHolder(@org.jetbrains.annotations.NotNull()
+    com.yo.silent_hours.ProfileListAdapter.ViewHolder holder, int position) {
+    }
+    
+    @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000&\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0002\u0018\u0000 \u000b2\u00020\u0001:\u0001\u000bB\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u000e\u0010\u0007\u001a\u00020\b2\u0006\u0010\t\u001a\u00020\nR\u000e\u0010\u0005\u001a\u00020\u0006X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\f"}, d2 = {"Lcom/yo/silent_hours/ProfileListAdapter$ViewHolder;", "Landroidx/recyclerview/widget/RecyclerView$ViewHolder;", "itemView", "Landroid/view/View;", "(Landroid/view/View;)V", "profileName", "Landroid/widget/TextView;", "bind", "", "item", "", "Companion", "app_debug"})
+    public static final class ViewHolder extends androidx.recyclerview.widget.RecyclerView.ViewHolder {
+        private final android.widget.TextView profileName = null;
+        @org.jetbrains.annotations.NotNull()
+        public static final com.yo.silent_hours.ProfileListAdapter.ViewHolder.Companion Companion = null;
+        
+        public ViewHolder(@org.jetbrains.annotations.NotNull()
+        android.view.View itemView) {
+            super(null);
+        }
+        
+        public final void bind(@org.jetbrains.annotations.NotNull()
+        java.lang.String item) {
+        }
+        
+        @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u0018\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u000e\u0010\u0003\u001a\u00020\u00042\u0006\u0010\u0005\u001a\u00020\u0006\u00a8\u0006\u0007"}, d2 = {"Lcom/yo/silent_hours/ProfileListAdapter$ViewHolder$Companion;", "", "()V", "create", "Lcom/yo/silent_hours/ProfileListAdapter$ViewHolder;", "parent", "Landroid/view/ViewGroup;", "app_debug"})
+        public static final class Companion {
+            
+            private Companion() {
+                super();
+            }
+            
+            @org.jetbrains.annotations.NotNull()
+            public final com.yo.silent_hours.ProfileListAdapter.ViewHolder create(@org.jetbrains.annotations.NotNull()
+            android.view.ViewGroup parent) {
+                return null;
+            }
+        }
+    }
+    
+    @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u0016\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u0014\u0010\u0003\u001a\b\u0012\u0004\u0012\u00020\u00050\u0004X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u0006"}, d2 = {"Lcom/yo/silent_hours/ProfileListAdapter$Companion;", "", "()V", "PROFILE_COMPARATOR", "Landroidx/recyclerview/widget/DiffUtil$ItemCallback;", "Lcom/yo/silent_hours/database/Profile;", "app_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/yo/silent_hours/database/ProfileRoomDatabase.java
@@ -0,0 +1,52 @@
+package com.yo.silent_hours.database;
+
+import java.lang.System;
+
+@androidx.room.Database(entities = {com.yo.silent_hours.database.Profile.class}, version = 1)
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u0014\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\b\'\u0018\u0000 \u00052\u00020\u0001:\u0001\u0005B\u0005\u00a2\u0006\u0002\u0010\u0002J\b\u0010\u0003\u001a\u00020\u0004H&\u00a8\u0006\u0006"}, d2 = {"Lcom/yo/silent_hours/database/ProfileRoomDatabase;", "Landroidx/room/RoomDatabase;", "()V", "profileDao", "Lcom/yo/silent_hours/database/ProfileDAO;", "Companion", "app_debug"})
+public abstract class ProfileRoomDatabase extends androidx.room.RoomDatabase {
+    @org.jetbrains.annotations.NotNull()
+    public static final com.yo.silent_hours.database.ProfileRoomDatabase.Companion Companion = null;
+    @kotlin.jvm.Volatile()
+    private static volatile com.yo.silent_hours.database.ProfileRoomDatabase INSTANCE;
+    
+    public ProfileRoomDatabase() {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public abstract com.yo.silent_hours.database.ProfileDAO profileDao();
+    
+    @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\b\u0086\u0003\u0018\u00002\u00020\u0001:\u0001\nB\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u0016\u0010\u0005\u001a\u00020\u00042\u0006\u0010\u0006\u001a\u00020\u00072\u0006\u0010\b\u001a\u00020\tR\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000\u00a8\u0006\u000b"}, d2 = {"Lcom/yo/silent_hours/database/ProfileRoomDatabase$Companion;", "", "()V", "INSTANCE", "Lcom/yo/silent_hours/database/ProfileRoomDatabase;", "getDatabase", "context", "Landroid/content/Context;", "scope", "Lkotlinx/coroutines/CoroutineScope;", "WordDatabaseCallback", "app_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+        
+        @org.jetbrains.annotations.NotNull()
+        public final com.yo.silent_hours.database.ProfileRoomDatabase getDatabase(@org.jetbrains.annotations.NotNull()
+        android.content.Context context, @org.jetbrains.annotations.NotNull()
+        kotlinx.coroutines.CoroutineScope scope) {
+            return null;
+        }
+        
+        @kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\b\u0002\u0018\u00002\u00020\u0001B\r\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004J\u0010\u0010\u0005\u001a\u00020\u00062\u0006\u0010\u0007\u001a\u00020\bH\u0016R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/yo/silent_hours/database/ProfileRoomDatabase$Companion$WordDatabaseCallback;", "Landroidx/room/RoomDatabase$Callback;", "scope", "Lkotlinx/coroutines/CoroutineScope;", "(Lkotlinx/coroutines/CoroutineScope;)V", "onCreate", "", "db", "Landroidx/sqlite/db/SupportSQLiteDatabase;", "app_debug"})
+        static final class WordDatabaseCallback extends androidx.room.RoomDatabase.Callback {
+            private final kotlinx.coroutines.CoroutineScope scope = null;
+            
+            public WordDatabaseCallback(@org.jetbrains.annotations.NotNull()
+            kotlinx.coroutines.CoroutineScope scope) {
+                super();
+            }
+            
+            /**
+             * Override the onCreate method to populate the database.
+             */
+            @java.lang.Override()
+            public void onCreate(@org.jetbrains.annotations.NotNull()
+            androidx.sqlite.db.SupportSQLiteDatabase db) {
+            }
+        }
+    }
+}
